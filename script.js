8d946e0b25bc62e8bf5f28ec2dacc8eb8e7f5cb8
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-05 17:05:47 +0100

    feat: created task km; task age; input

---
+++ script.js
@@ -0,0 +1,53 @@
+
+// // Chiedo all'utente i chilometri da percorrere
+// const milesToGo = ('Inserisci i chilometri di distanza che ti separano dalla tua destinazione');
+// const userAge = ('Inserisci la tua Età');
+
+// // Converto i valori in numeri
+// let quantityMiles = parseFloat(milesToGo);
+// let quantityAge = parseInt(userAge);
+
+// // Controllo validità dei dati
+// if (isNaN(quantityMiles) || isNaN(quantityAge)) {
+//     alert("I dati inseriti non sono validi");
+// } else {
+//     // Tariffa per KM
+//     const costForMiles = 0.21;
+
+//     // Calcolo il prezzo del biglietto
+//     let costForTicket = costForMiles * quantityMiles;
+
+//     // Sconto in base all'età
+//     let discountPercentage = 0;
+
+//     if (quantityAge < 18) {
+//         discountPercentage = 20;
+//     } else if (quantityAge >= 65) {
+//         discountPercentage = 40;
+//     }
+
+//     // Calcolo dello sconto
+//     let discountAmount = (costForTicket * discountPercentage) / 100;
+//     costForTicket -= discountAmount;
+
+//     // Formattazione del prezzo
+//     costForTicket = Number(costForTicket.toFixed(2));
+
+//     // Risultato
+//     alert(`Il prezzo totale del biglietto è: €${costForTicket}`);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
