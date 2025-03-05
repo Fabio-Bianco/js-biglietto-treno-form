4973679859e9c0e4f151c02c8ae803ad13c34eb8
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-05 18:20:05 +0100

    feat: calc of the cost of the ticket

--- script.js
+++ script.js
@@ -31,12 +31,23 @@ sendTaskElement.addEventListener('click', function(event) {
 
     console.log("Valore chilometri:", kmValue);
     console.log("Valore età:", ageValue);
+    
     // Converto i valori persi dall'utente in numeri
     let quantityMiles = parseFloat(kmValue);
     let quantityAge = parseInt(ageValue);
 
     console.log("Chilometri (convertiti):", quantityMiles);
     console.log("Età (convertita):", quantityAge);
+
+   /** Controllo validità dei dati;
+    *  isNaN verifica se il valore inserito non è un numero o è minore o uguale a 0;
+    * document.getElementById("result").innerHTML modifica il contenuto della pagina HTML (l'elemento con id result) per mostrare un messaggio di errore visibile all'utente, informandolo che i dati non sono validi.
+    * */
+    if (isNaN(quantityMiles) || isNaN(quantityAge) || quantityMiles <= 0 || quantityAge <= 0 || quantityAge > 120) {
+        console.log("I dati inseriti non sono validi.");
+        document.getElementById("result").innerHTML = "<p class='text-danger'>I dati inseriti non sono validi.</p>";
+        return;
+    }
 });
 
 
